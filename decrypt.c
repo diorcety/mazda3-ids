37535668e3cc9becda243d14c567951179f21fa1
Yann Diorcet <diorcet.yann@gmail.com>
2016-05-07 11:46:34 +0200

    Reformat C code

--- decrypt.c
+++ decrypt.c
@@ -11,65 +11,65 @@
 #define TRY_FREE(b) {if(b!=NULL){free(b);}}
 
 long int get_file_size(FILE *file) {
-  fseek(file, 0L, SEEK_END);
-  long int l = ftell(file);
-  fseek(file, 0L, SEEK_SET);
-  return l;
+    fseek(file, 0L, SEEK_END);
+    long int l = ftell(file);
+    fseek(file, 0L, SEEK_SET);
+    return l;
 }
 
 int main(int argc, char *argv[]) {
-  int ret = 0;
-  if (argc != 3) {
-    fprintf(stderr, "No arg\n");
-    return -1;
-  }
-  FILE *password_file = fopen(argv[1], "rb");
-  long int password_size = get_file_size(password_file);
-  char *password = (char *)malloc(password_size);
-  fread(password, 1, password_size, password_file);
-  fclose(password_file);
+    int ret = 0;
+    if (argc != 3) {
+        fprintf(stderr, "No arg\n");
+        return -1;
+    }
+    FILE *password_file = fopen(argv[1], "rb");
+    long int password_size = get_file_size(password_file);
+    char *password = (char *) malloc(password_size);
+    fread(password, 1, password_size, password_file);
+    fclose(password_file);
 
-  FILE *file = fopen(argv[2], "rb");
-  long int file_size = get_file_size(file);
-  char *content = (char *)malloc(file_size);
-  fread(content, 1, file_size, file);
-  fclose(file);
+    FILE *file = fopen(argv[2], "rb");
+    long int file_size = get_file_size(file);
+    char *content = (char *) malloc(file_size);
+    fread(content, 1, file_size, file);
+    fclose(file);
 
-  EVP_CIPHER_CTX ctx;
-  EVP_CIPHER_CTX_init(&ctx);
-  const EVP_CIPHER* cipher = EVP_des_ede3_cbc();
-  const EVP_MD* md = EVP_md5();
+    EVP_CIPHER_CTX ctx;
+    EVP_CIPHER_CTX_init(&ctx);
+    const EVP_CIPHER *cipher = EVP_des_ede3_cbc();
+    const EVP_MD *md = EVP_md5();
 
-  char salt[8];
-  memcpy(salt, content + 8, 8);
-  char * key = (char *)malloc(32);
-  char * iv = (char *)malloc(16);
-  char * out = NULL;
-  EVP_CHECK(EVP_BytesToKey(cipher, md, salt, password, password_size, 1, key, iv), 24);
+    char salt[8];
+    memcpy(salt, content + 8, 8);
+    char *key = (char *) malloc(32);
+    char *iv = (char *) malloc(16);
+    char *out = NULL;
+    EVP_CHECK(EVP_BytesToKey(cipher, md, salt, password, password_size, 1, key, iv), 24);
 
-  EVP_CHECK(EVP_DecryptInit_ex(&ctx, cipher, 0, key, iv), 1);
-  size_t ctxbz = EVP_CIPHER_CTX_block_size(&ctx);
+    EVP_CHECK(EVP_DecryptInit_ex(&ctx, cipher, 0, key, iv), 1);
+    size_t ctxbz = EVP_CIPHER_CTX_block_size(&ctx);
 
-  int outl;
-  out = (char *)malloc(file_size + ctxbz);
-  EVP_CHECK(EVP_DecryptUpdate(&ctx, out, &outl, content + 0x10, file_size - 0x10), 1);
+    int outl;
+    out = (char *) malloc(file_size + ctxbz);
+    EVP_CHECK(EVP_DecryptUpdate(&ctx, out, &outl, content + 0x10, file_size - 0x10), 1);
 
-  int outl2;
-  EVP_CHECK(EVP_DecryptFinal_ex(&ctx, out + outl, &outl2), 1);
+    int outl2;
+    EVP_CHECK(EVP_DecryptFinal_ex(&ctx, out + outl, &outl2), 1);
 
-  EVP_CHECK(EVP_CIPHER_CTX_cleanup(&ctx), 1);
+    EVP_CHECK(EVP_CIPHER_CTX_cleanup(&ctx), 1);
 
-  fwrite(out, 1, outl + outl2, stdout);
-  fflush(stdout);
-  goto exit;
+    fwrite(out, 1, outl + outl2, stdout);
+    fflush(stdout);
+    goto exit;
 
-fail:
-  ret = 1;
-exit:
-  TRY_FREE(password);
-  TRY_FREE(key);
-  TRY_FREE(iv);
-  TRY_FREE(out);
-  free(content);
-  return ret;
+    fail:
+    ret = 1;
+    exit:
+    TRY_FREE(password);
+    TRY_FREE(key);
+    TRY_FREE(iv);
+    TRY_FREE(out);
+    free(content);
+    return ret;
 }
--- exml.c
+++ exml.c
@@ -6,35 +6,35 @@
 #include <stdlib.h>
 
 void encrypt(char *a1, unsigned int size) {
-  int i;
-  for (i = 0; i < size; ++i ) {
-    a1[i] = ((a1[i] & 0xF) << 4) | ((a1[i] & 0xF0) >> 4);
-  }
+    int i;
+    for (i = 0; i < size; ++i) {
+        a1[i] = ((a1[i] & 0xF) << 4) | ((a1[i] & 0xF0) >> 4);
+    }
 }
 
 long int get_file_size(FILE *file) {
-  fseek(file, 0L, SEEK_END);
-  long int l = ftell(file);
-  fseek(file, 0L, SEEK_SET);
-  return l;
+    fseek(file, 0L, SEEK_END);
+    long int l = ftell(file);
+    fseek(file, 0L, SEEK_SET);
+    return l;
 }
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "No arg\n");
-    return -1;
-  }
-  FILE *file = fopen(argv[1], "rb");
-  long int file_size = get_file_size(file);
-  char *content = (char *)malloc(file_size);
-  fread(content, 1, file_size, file);
-  fclose(file);
+    if (argc != 2) {
+        fprintf(stderr, "No arg\n");
+        return -1;
+    }
+    FILE *file = fopen(argv[1], "rb");
+    long int file_size = get_file_size(file);
+    char *content = (char *) malloc(file_size);
+    fread(content, 1, file_size, file);
+    fclose(file);
 
-  encrypt(content, file_size);
+    encrypt(content, file_size);
 
-  fwrite(content, 1, file_size, stdout);
-  fflush(stdout);
+    fwrite(content, 1, file_size, stdout);
+    fflush(stdout);
 
-  free(content);
-  return 0;
+    free(content);
+    return 0;
 }
