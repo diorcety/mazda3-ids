51ac7400fa895ae39d3c431d80e96c2c9c020327
Yann Diorcet <diorcet.yann@gmail.com>
2016-07-17 11:15:20 +0200

    Add license headers

--- fnp.c
+++ fnp.c
@@ -1,7 +1,19 @@
 /*
- * Copyright (C) 2016 Yann Diorcet
- * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
- */
+  Copyright (C) 2016 Yann Diorcet
+
+  This file is part of IDS.  IDS is free software: you can
+  redistribute it and/or modify it under the terms of the GNU General Public
+  License as published by the Free Software Foundation, version 2.
+ 
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+ 
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc., 51
+  Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 #include <windows.h>
 #include <stdio.h>
 
